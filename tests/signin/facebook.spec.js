edb1441f2dd4b60055009b467711a69b29cf37de
mubeenhassan <mubeenhassan34@gmail.com>
2022-05-30 17:19:30 +0500

    adding facebook login

--- tests/signin/facebook.spec.js
+++ tests/signin/facebook.spec.js
@@ -5,12 +5,14 @@ test('Signin With Google', async ({ page }) => {
   await page.locator('text=Sign In').click();
   const [page1] = await Promise.all([
     page.waitForEvent('popup'),
-    page.frameLocator('iframe').locator('button:has-text("Continue with Google")').click()
+    page.frameLocator('iframe').locator('button:has-text("Continue with Facebook")').click()
   ]);
-  await page1.locator('[aria-label="Email or phone"]').fill(process.env.GOOGLE_EMAIL);
-  page1.locator('[aria-label="Email or phone"]').press('Enter')
-  await page1.locator('[aria-label="Enter your password"]').fill(process.env.GOOGLE_PASSWORD);
-  page1.locator('#passwordNext button:has-text("Next")').click()
+  await page1.locator('[placeholder="Email address or phone number"]').fill(process.env.FACEBOOK_USERNAME);
+  await page1.locator('[placeholder="Email address or phone number"]').press('Tab');
+  await page1.locator('[placeholder="Password"]').fill(process.env.FACEBOOK_PASSWORD);
+  await Promise.all([
+    page1.locator('[placeholder="Password"]').press('Enter')
+  ]);
   await page.locator('text=My wallet').click();
   await page.screenshot({ path: `./screenshots/${Date.now()}signin.png` })
 })
